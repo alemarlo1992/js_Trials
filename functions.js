0cfb9b9b03897251546d2f022173279629c94d04
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-24 17:02:36 -0700

    update calculateTotal function

--- functions.js
+++ functions.js
@@ -15,8 +15,25 @@ const getFullName = (firstName, lastName) => {
 
 }
 
-/** 3. calculateTotal */
+const calculateTotal = (basePrice, state, tax = 0.05) => {
+  // const tax = 0.05
+  let subtotal = basePrice * (1 + tax);
+  let fee = 0;
+  if (state === 'CA') {
+    fee === 0.03 * subtotal;
+  }else if (state === 'PA') {
+    fee === 2;
+  }else if (state === 'MA') {
+      if (basePrice <= 100) {
+        fee === 1;
+      }else{
+        fee == 3
+      }
+  }
+  return subtotal + fee
+}
 
 // Define your function here;
 isHometown('San Diego');
 getFullName('Erica', 'Alejandra')
+calculateTotal(150, 'CA')
