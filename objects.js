82f05371844f0c6f5b9a48271b36a91ffad70bc0
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-24 17:30:25 -0700

    update countWords(phrase) and getMelonsAtPrice

--- objects.js
+++ objects.js
@@ -3,11 +3,34 @@
 
 /** 1. countWords */
 function countWords(phrase) {
-	// Replace this with your code
+  let wordCounts = {};
+  for (let word of phrase.split(' ')) {
+    if (wordCounts[word]) {
+      wordCounts[word] += 1 
+    }else{
+      wordCounts[word] = 1
+    }
+
+  }
+  return wordCounts
 }
 
 
-/** 2. getMelonsAtPrice */
 function getMelonsAtPrice(price) {
-	// Replace this with your code
+  let melonPrices = {
+        2.50: ['Cantaloupe', 'Honeydew'],
+        2.95: ['Watermelon'],
+        3.25: ['Musk', 'Crenshaw'],
+        14.25: ['Christmas']
+  }
+  if (price === undefined) {
+    return NONE;
+  }else {
+    let sortPrice = melonPrices[price].sort();
+    return sortPrice
+  }
+
 }
+
+countWords('Wet foot, dry foot. Low foot, high foot.')
+getMelonsAtPrice(3.25)
